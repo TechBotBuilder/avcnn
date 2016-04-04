c3c9898e061cb9e90b99967973e6b91750cc3c84
TechBotBuilder <techbotbuilder@gmail.com>
2016-04-04 00:11:16 -0400

    Author Notice and rc.local warning

--- startup.py
+++ startup.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+#This script is originally by Maxwell Budd (TechBotBuilder).
+#Please credit me as one or the other if you find this work helpful or derive code from it.
+
+#this script should be setup to run in /etc/rc.local right before exit(0)
+#with the line "python3 {path_to_avc_dir}/startup.sh &"
+#but don't do that unless you have a switch going from https://pinout.xyz/pinout/pin16_gpio23 to gnd
+#or else you won't be able to boot up normally.
+
 from controller import main
 import RPi.GPIO as GPIO
 
