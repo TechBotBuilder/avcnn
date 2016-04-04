ad0be79c8ed78981577102174b4916e82f4586eb
TechBotBuilder <techbotbuilder@gmail.com>
2016-04-04 00:30:40 -0400

    Create get_requirements.sh
    this file gets all the requirements for running avcnn

---
+++ get_requirements.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#this file gets all the requirements for running avcnn
+cd ~
+sudo pip3 install pynmea2 dill
+mkdir piblaster && cd piblaster
+wget https://github.com/sarfarta/pi-blaster/archive/master.zip
+unzip master.zip
+rm master.zip
+cd pi-blaster-master
+./autogen.sh
+./configure && make
+sudo make install
+
+cd ~
+sudo apt-get install python3-scipy
+sudo pip3 install Theano
+sudo pip3 install keras
+sudo apt-get install libhdf5-dev
+sudo HDF5_DIR=/usr/lib/arm-linux-gnueabihf/hdf5/serial CC=hdcc pip3 install h5py
+
+cd ~
+mkdir avc && cd avc
+wget http://techbotbuilder.com/avcnn/code/raspi_code.zip
+unzip raspi_code.zip
+rm raspi_code.zip
