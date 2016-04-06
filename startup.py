a888da5002b6c4ae011ab7ab602b66b5cbe6d9ca
Maxwell Budd <techbotbuilder@gmail.com>
2016-04-05 20:05:57 -0400

    set RPI.GPIO mode; move import
    GPIO needs a mode to work.
    import robot controller should be in the conditional statement because if we don't want to run the script, why import it?

--- startup.py
+++ startup.py
@@ -7,12 +7,13 @@
 #but don't do that unless you have a switch going from https://pinout.xyz/pinout/pin16_gpio23 to gnd
 #or else you won't be able to boot up normally.
 
-from controller import main
 import RPi.GPIO as GPIO
 
+GPIO.setmode(GPIO.BOARD)
 startswitchpin = 16
 GPIO.setup(startswitchpin, GPIO.IN, pull_up_down=GPIO.PUD_UP)#normally connected to +3 v
 if GPIO.input(startswitchpin) == 1:
     #if the switch is not closed
+    from controller import main
     main()#run the program
 #otherwise, if someone has closed the switch->0v, just do nothing
